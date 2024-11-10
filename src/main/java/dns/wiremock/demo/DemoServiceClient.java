124800f7453153547ee44ac28e29085aa98ded32
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2024-11-10 19:41:52 +0100

    Add more wiremock demos

--- src/main/java/dns/wiremock/demo/DemoServiceClient.java
+++ src/main/java/dns/wiremock/demo/DemoServiceClient.java
@@ -1,6 +1,8 @@
 package dns.wiremock.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -15,6 +17,26 @@ public class DemoServiceClient {
     }
 
     public String getSimpleResponseDemo() {
-        return restClient.get().uri("/simple-response").retrieve().body(String.class);
+        return restClient.get().uri("/api/simple-response").retrieve().body(String.class);
+    }
+
+    public String getEchoDemo(String id) {
+        return restClient.get().uri("/api/echo/%s".formatted(id)).retrieve().body(String.class);
+    }
+
+    public String createJwtDemo() {
+        return restClient.get().uri("/api/jwt").retrieve().body(String.class);
+    }
+
+    public void timeoutDemo() {
+        restClient.get().uri("/api/timeout").retrieve().toBodilessEntity();
+    }
+
+    public HttpStatusCode stubHttpStatusDemo(HttpStatus status) {
+        return restClient.get().uri("/api/http-status/%d".formatted(status.value())).retrieve().toBodilessEntity().getStatusCode();
+    }
+
+    public String importBodyFileDemo() {
+        return restClient.get().uri("/api/import-body-file").retrieve().body(String.class);
     }
 }
--- src/test/java/dns/wiremock/demo/DemoServiceClientTest.java
+++ src/test/java/dns/wiremock/demo/DemoServiceClientTest.java
@@ -3,20 +3,29 @@ package dns.wiremock.demo;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.wiremock.spring.ConfigureWireMock;
 import org.wiremock.spring.EnableWireMock;
 import org.wiremock.spring.InjectWireMock;
 
+import java.util.UUID;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @EnableWireMock({
-        @ConfigureWireMock(name = "demo-service", baseUrlProperties = "demo-service.base-url", port = 8045)
+        @ConfigureWireMock(name = "demo-service",
+                filesUnderClasspath = "wiremock/demo-service",
+                baseUrlProperties = "demo-service.base-url",
+                port = 8045)
 })
 @SpringBootTest
 class DemoServiceClientTest {
@@ -31,12 +40,12 @@ class DemoServiceClientTest {
     private WireMockServer demoServiceWireMock;
 
     @Test
-    void simpleWiremockServiceResponse() throws JSONException {
+    void manualWiremockStub() throws JSONException {
         demoServiceWireMock.stubFor(
-                get("/simple-response")
+                get("/api/simple-response")
                         .willReturn(aResponse()
                                 .withStatus(HttpStatus.OK.value())
-                                .withBodyFile("demo-service/simple-response.json")
+                                .withBodyFile("simple-response.json")
                         ));
 
         String response = demoServiceClient.getSimpleResponseDemo();
@@ -47,5 +56,25 @@ class DemoServiceClientTest {
                           "lastName": "TheLastName"
                         }
                 """, response, true);
+    }
+
+    @Test
+    void echoDemoUsingAutomaticMappings() throws JSONException {
+        String id = UUID.randomUUID().toString();
+        String response = demoServiceClient.getEchoDemo(id);
+        JSONAssert.assertEquals(response, "{\"echo\":\"%s\"}".formatted(id), true);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = HttpStatus.class, names = {"OK", "NO_CONTENT"})
+    void httpStatusCodeDemoUsingAutomaticMappings(HttpStatus status) {
+        HttpStatusCode response = demoServiceClient.stubHttpStatusDemo(status);
+        assertThat(response).isEqualTo(status);
+    }
+
+    @Test
+    void importBodyFileDemoUsingAutomaticMappings() throws JSONException {
+        String response = demoServiceClient.importBodyFileDemo();
+        JSONAssert.assertEquals(response, "{\"description\":\"Import body file in Wiremock\"}", true);
     }
 }
